85ee0fa196348171ac15c6811b3b7aa2834b5c0b
aharshsingh <aharshsingh25@gmail.com>
2025-07-01 16:57:42 +0530

    workspace

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-function Home() {
-  return <h1 className="text-3xl font-bold text-start mt-10">Welcome to BiteSpeed Flow Builder</h1>;
-}
+import Workspace from './page/Workspace'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Workspace />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend-app/src/page/Workspace.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from "react";
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  BackgroundVariant,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function Workspace() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh",  background: 'linear-gradient(135deg, #e0f7fa, #e1bee7)', }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background
+          variant={BackgroundVariant.Dots}
+color="#691b9aa7"
+          gap={30}
+          size={3}
+        />
+      </ReactFlow>
+    </div>
+  );
+}
